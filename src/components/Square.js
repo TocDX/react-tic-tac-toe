18a6dc99554a6b1930eb651a29e03725e8e19b92
TocDx <sk372021000@gmail.com>
2023-11-15 19:06:30 +0900

    새로운 폴더

---
+++ src/components/Square.js
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+import './Square.css';
+
+const Square = ({ onClick, value }) => {
+  return (
+    <button
+      className="square"
+      onClick={() => {
+        onClick();
+      }}
+    >
+      {value}
+    </button>
+  );
+};
+
+export default Square;
